a4526a44096514566145289e01ceadc7c4d0be35
John R. O'Donnell <matadorcrab@gmail.com>
2023-12-19 10:10:43 -0600

    need someone @Moosius to fix my bad concurrency

--- tests/tests/src/main/java/org/example/Main.java
+++ tests/tests/src/main/java/org/example/Main.java
@@ -5,29 +5,30 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.function.Consumer;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 
 
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException, InterruptedException, ExecutionException, TimeoutException {
         System.out.println("Hello world!");
         notNotOdd();
     }
 
-    public static boolean notNotOdd() throws IOException, InterruptedException {
+    public static void notNotOdd() throws IOException, InterruptedException, ExecutionException, TimeoutException {
         ExecutorService executor = Executors.newFixedThreadPool(2);
         ProcessBuilder  p = new ProcessBuilder().command(new String[]{"node", "notNotOddNegCase.js"});
         Process process = p.start();
         StreamRead stream = new StreamRead(process.getInputStream(),System.out::println);
         int exit = process.waitFor();
         Future<?> future = executor.submit(stream);
-        System.out.println(future);
+        System.out.println(future.get(10, TimeUnit.SECONDS));
         
-
-        return true;
     };
 
     private static class StreamRead implements Runnable {
