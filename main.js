0cffdad0d47daeac8d637ee0e03e17e4d260ce56
lucas sprouole <lucas.sproule.42@gmail.com>
2023-12-19 02:25:37 -0500

    war crimes

--- main.js
+++
@@ -1,5 +0,0 @@
-import { notNotOdd, notNotNotOdd, notNotNotNotNotNotNotNotNotNotNotNotNotNotNotNotNotNotNotNotNotNotNotNotNotNotNotNotNotOdd } from './index.js';
-
-console.log(notNotOdd(2)); // false
-console.log(notNotNotOdd(2)); // true
-console.log(notNotNotNotNotNotNotNotNotNotNotNotNotNotNotNotNotNotNotNotNotNotNotNotNotNotNotNotNotOdd(2))
