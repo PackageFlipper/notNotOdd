97c44264ab9c645597ebfde1b0e58b953d592406
John R. O'Donnell <matadorcrab@gmail.com>
2023-12-19 16:14:33 -0600

    started rust implementation

---
+++ build_time/target/debug/deps/build_time-611e1cff2db97e11.d
@@ -0,0 +1,5 @@
+/home/johnrodonnell/notOdd/notNotOdd/build_time/target/debug/deps/build_time-611e1cff2db97e11.rmeta: src/main.rs
+
+/home/johnrodonnell/notOdd/notNotOdd/build_time/target/debug/deps/build_time-611e1cff2db97e11.d: src/main.rs
+
+src/main.rs:
