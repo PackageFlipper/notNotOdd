71f7b519dd4ec46c8e4ca8f6b30eddda908e6a43
John R. O'Donnell <matadorcrab@gmail.com>
2023-12-19 12:05:25 -0600

    idk

--- tests/tests/src/main/java/org/example/Main.java
+++ tests/tests/src/main/java/org/example/Main.java
@@ -1,20 +1,27 @@
 package org.example;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 
 
+
 public class Main {
+  
+
+
     public static void main(String[] args) throws IOException, InterruptedException, ExecutionException, TimeoutException {
         System.out.println("Hello world!");
         notNotOdd();
@@ -26,6 +33,7 @@ public class Main {
         Process process = p.start();
         //this line needs to be solved. consumer is wrong
         StreamRead stream = new StreamRead(process.getInputStream(),System.out::println);
+        //  String result = IOUtils.toString(stream, StandardCharsets.UTF_8);
         
         Future<?> future = executor.submit(stream);
         int exit = process.waitFor();
@@ -48,5 +56,6 @@ public class Main {
         }
 
     }
+
 
 }
