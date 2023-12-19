18f8dceeb93c3970a0ae2a0edaf805cbaee2519c
John R. O'Donnell <matadorcrab@gmail.com>
2023-12-19 09:33:15 -0600

    started tests

--- tests/tests/src/main/java/org/example/Main.java
+++ tests/tests/src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
+        notNotOdd();
     }
+
+    public static boolean notNotOdd() throws IOException {
+        Process p = Runtime.getRuntime().exec(new String[]{"node", "notNotOddNegCase.js"});
+        System.out.println(p);
+
+        return true;
+    };
+
 }
