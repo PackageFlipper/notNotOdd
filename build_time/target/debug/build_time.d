17bff1c1c5425aed97e0d4b7b5a5b33243f3f4c3
John R. O'Donnell <matadorcrab@gmail.com>
2023-12-19 22:23:10 -0600

    started maven build

---
+++ build_time/target/debug/build_time.d
@@ -0,0 +1,1 @@
+/home/johnrodonnell/notOdd/notNotOdd/build_time/target/debug/build_time: /home/johnrodonnell/notOdd/notNotOdd/build_time/src/main.rs
---
+++ build_time/target/debug/deps/build_time-1a4ec5e037045756.d
@@ -0,0 +1,5 @@
+/home/johnrodonnell/notOdd/notNotOdd/build_time/target/debug/deps/build_time-1a4ec5e037045756: src/main.rs
+
+/home/johnrodonnell/notOdd/notNotOdd/build_time/target/debug/deps/build_time-1a4ec5e037045756.d: src/main.rs
+
+src/main.rs:
