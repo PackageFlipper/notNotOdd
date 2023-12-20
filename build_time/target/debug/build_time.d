f1ce72bc64fea40ed92517ad9d7602c45aa9f18d
John R. O'Donnell <matadorcrab@gmail.com>
2023-12-20 13:15:59 -0600

    na

--- build_time/target/debug/build_time.d
+++ build_time/target/debug/build_time.d
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 /home/johnrodonnell/notOdd/notNotOdd/build_time/target/debug/build_time: /home/johnrodonnell/notOdd/notNotOdd/build_time/src/main.rs
+=======
+/tmp/notNotOdd/build_time/target/debug/build_time: /tmp/notNotOdd/build_time/build.rs /tmp/notNotOdd/build_time/src/main.rs
+>>>>>>> adf34d60ef177ad5ba74bf351071c4de91914647
---
+++ build_time/target/debug/deps/build_time-a94d51ae7fb98483.d
@@ -0,0 +1,5 @@
+/tmp/notNotOdd/build_time/target/debug/deps/build_time-a94d51ae7fb98483: src/main.rs
+
+/tmp/notNotOdd/build_time/target/debug/deps/build_time-a94d51ae7fb98483.d: src/main.rs
+
+src/main.rs:
